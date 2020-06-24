6f180f6f7805c123d0e04807ed161c03faba1d69
n20028 <n20028@std.it-clollege.ac.jp>
2020-06-24 11:53:31 +0900

    file pattern

--- findgrep.sh
+++ findgrep.sh
@@ -2,10 +2,14 @@
 
 pattern=$1
 directory=$2
+name=$3
 if [ -z "$directory" ]; then
 	directory='.'
 fi
+if [ -z "$name" ]; then
+	name='*'
+fi
 # -n : print line number
 # -H : print the file name
-find "$directory" -type f | xargs grep -nH "$pattern"
+find "$directory" -type f -name "$name" | xargs grep -nH "$pattern"
 
